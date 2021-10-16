8054c48849f2ab1bfd3488ffb6ae208fdafce077
arpit-zibma <72298268+arpit-zibma@users.noreply.github.com>
2021-10-16 16:51:27 +0530

    16-10 (FireXcode Implement)

--- lib/modules/bottombar_container.dart
+++ lib/modules/bottombar_container.dart
@@ -13,9 +13,9 @@ class BottomBarContainer extends StatelessWidget {
   Widget build(BuildContext context) {
     return xColumnCC
         .list([
-          icons.xIcons(color: Colors.white),
+          icons!.xIcons(color: Colors.white),
           4.0.sizedHeight(),
-          title.xText(color: Colors.white)
+          title!.xText(color: Colors.white)
         ])
         .xInkWell(onTap: ontap)
         .xMaterial(
